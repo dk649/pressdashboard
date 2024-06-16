5b96c7a15533ae6b25ea2a857ea7f4ee0e41959f
dk649 <58308212+dk649@users.noreply.github.com>
2024-06-16 14:35:23 +0100

    updates
    added table

--- components/ui/posts/PostTable.tsx
+++ components/ui/posts/PostTable.tsx
@@ -5,13 +5,69 @@ import {
   TableHead,
   TableHeader,
   TableRow,
+  TableCaption,
 } from "@/components/ui/table";
 
 import posts from "@/data/posts";
 import { Post } from "@/types/posts";
+import Link from "next/link";
 
-const PostsTable = () => {
-  return <div>posts</div>;
+interface PostsTableProps {
+  limit?: number;
+  title?: string;
+}
+
+const PostsTable = ({ limit, title }: PostsTableProps) => {
+  //sort posts in descending order
+
+  const sortedPosts: Post[] = [...posts].sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
+  //filter posts to limit
+
+  const filteredPosts = limit ? sortedPosts.slice(0, limit) : sortedPosts;
+
+  return (
+    <div className="mt-10 ">
+      <h3 className="text-2xl mb-4 fpnt-semibold">{title ? title : "Posts"}</h3>
+
+      <Table>
+        <TableCaption>A list of recent posts</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+            <TableHead className="hidden md:table-cell">Author</TableHead>
+            <TableHead className="hidden md:table-cell text-right">
+              Date
+            </TableHead>
+            <TableHead>View</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {filteredPosts.map((post) => (
+            <TableRow key={post.id}>
+              <TableCell>{post.title}</TableCell>
+              <TableCell className="hidden md:table-cell">
+                {post.author}
+              </TableCell>
+              <TableCell className="hidden md:table-cell text-right">
+                {post.date}
+              </TableCell>
+
+              <TableCell>
+                <Link href={`/posts/edit/${post.id}`}>
+                  <button className="bg-green-500 hover:bg-green-700 text-black font-bold p-2 rounded-lg">
+                    Edit
+                  </button>
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 export default PostsTable;
